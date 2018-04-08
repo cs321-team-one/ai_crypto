826ca0a95717b3bc58ba9ddff4545a260c9812af
oleg menyaylenko <omenyayl@gmu.edu>
2018-04-08 12:45:39 -0700

    - Cant believe getting the search bar right is taking so long...

--- client/main.js
+++ client/main.js
@@ -26,35 +26,35 @@ Template.HomeLayout.rendered = function() {
 
     // Search bar config
 
-    let trig = 1;
-    const searchBar = $('.expandable-search');
-    searchBar.addClass('searchbarfix');
-    //animate searchbar width increase to  +150%
-    searchBar.click(function(e) {
-        //handle other nav elements visibility here to avoid push down
-        $('.search-hide').addClass('hide');
-        if (trig === 1){
-            $('#navfix2').animate({
-                width: '+=150',
-                marginRight: 0
-            }, 400);
-
-            trig ++;
-        }
-
-    });
-
-    // if user leaves the form the width will go back to original state
-
-    searchBar.focusout(function() {
-
-        $('#navfix2').animate({
-            width: '-=150'
-        }, 400);
-        trig = trig - 1;
-        //handle other nav elements visibility first to avoid push down
-        $('.search-hide').removeClass('hide');
-    });
+    // let trig = 1;
+    // const searchBar = $('.expandable-search');
+    // searchBar.addClass('searchbarfix');
+    // //animate searchbar width increase to  +150%
+    // searchBar.click(function(e) {
+    //     //handle other nav elements visibility here to avoid push down
+    //     $('.search-hide').addClass('hide');
+    //     if (trig === 1){
+    //         $('#navfix2').animate({
+    //             width: '+=150',
+    //             marginRight: 0
+    //         }, 400);
+    //
+    //         trig ++;
+    //     }
+    //
+    // });
+    //
+    // // if user leaves the form the width will go back to original state
+    //
+    // searchBar.focusout(function() {
+    //
+    //     $('#navfix2').animate({
+    //         width: '-=150'
+    //     }, 400);
+    //     trig = trig - 1;
+    //     //handle other nav elements visibility first to avoid push down
+    //     $('.search-hide').removeClass('hide');
+    // });
 
 };
 
