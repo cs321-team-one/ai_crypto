e296a96048dd8cbcb077795768092efe6074cb96
oleg menyaylenko <omenyayl@gmu.edu>
2018-04-08 15:02:58 -0700

    - worked on the News template with some cool shit for mobile devices. Inserted some mock data
    - sidenav fix

--- client/main.js
+++ client/main.js
@@ -15,7 +15,9 @@ const supportedCryptocurrencies = {
 };
 
 Template.HomeLayout.rendered = function() {
-    $(".button-collapse-sidenav").sideNav();
+    $(".button-collapse-sidenav").sideNav({
+        closeOnClick: true
+    });
     $('input.autocomplete').autocomplete({
         data: supportedCryptocurrencies,
         onAutocomplete: function(txt){
@@ -24,41 +26,9 @@ Template.HomeLayout.rendered = function() {
         }
     });
 
-    // Search bar config
-
-    // let trig = 1;
-    // const searchBar = $('.expandable-search');
-    // searchBar.addClass('searchbarfix');
-    // //animate searchbar width increase to  +150%
-    // searchBar.click(function(e) {
-    //     //handle other nav elements visibility here to avoid push down
-    //     $('.search-hide').addClass('hide');
-    //     if (trig === 1){
-    //         $('#navfix2').animate({
-    //             width: '+=150',
-    //             marginRight: 0
-    //         }, 400);
-    //
-    //         trig ++;
-    //     }
-    //
-    // });
-    //
-    // // if user leaves the form the width will go back to original state
-    //
-    // searchBar.focusout(function() {
-    //
-    //     $('#navfix2').animate({
-    //         width: '-=150'
-    //     }, 400);
-    //     trig = trig - 1;
-    //     //handle other nav elements visibility first to avoid push down
-    //     $('.search-hide').removeClass('hide');
-    // });
-
 };
 
-Template.HomeLayout.onCreated(function helloOnCreated() {
+Template.HomeLayout.onCreated(function () {
     Session.set('cryptocurrecySelection', 'Bitcoin (BTC)');
 });
 
@@ -76,5 +46,53 @@ Template.HomeLayout.events({
         if(formVal in supportedCryptocurrencies){
             Session.set('cryptocurrecySelection', formVal);
         }
+    }
+});
+
+Template.News.helpers({
+    newsArticles : function(){
+        try{
+            const newsResponse = HTTP.call('GET', NEWS_API);
+            console.log(newsResponse);
+        } catch (e) {
+            console.error('Cannot get news from the news api.')
+        }
+
+        const MAX_CARDS_PER_ROW = 4;
+
+        dataArr = [
+            {
+                title: 'News 1',
+                url: 'http://news1.com',
+                body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                imageurl: "https://images.cryptocompare.com/news/livebitcoinnews/c20cw000800.png"
+            },
+            {
+                title: 'News 2',
+                url: 'http://news2.com',
+                body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                imageurl: "https://images.cryptocompare.com/news/bitcoinist/aMNqsQogN90.jpeg"
+            },
+            {
+                title: 'News 3',
+                url: 'http://news3.com',
+                body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                imageurl: "https://images.cryptocompare.com/news/bitcoinist/aMNqsQogN90.jpeg"
+            },
+            {
+                title: 'News 4',
+                url: 'http://news4.com',
+                body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                imageurl: "https://images.cryptocompare.com/news/bitcoinist/aMNqsQogN90.jpeg"
+            }
+        ];
+
+        dataArr.forEach((datum, i)=>{
+            datum['span'] = `span_${(i%MAX_CARDS_PER_ROW)+1}_of_${MAX_CARDS_PER_ROW}`
+        });
+
+        console.log(dataArr);
+
+        return dataArr;
     }
 });
