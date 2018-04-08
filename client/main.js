efd9516272ae9045c79693f7259644894a04f44f
oleg menyaylenko <omenyayl@gmu.edu>
2018-04-08 11:00:13 -0700

    - Fixed search bar

--- client/main.js
+++ client/main.js
@@ -18,7 +18,44 @@ Template.HomeLayout.rendered = function() {
     $(".button-collapse-sidenav").sideNav();
     $('input.autocomplete').autocomplete({
         data: supportedCryptocurrencies,
+        onAutocomplete: function(txt){
+            Session.set('cryptocurrecySelection', txt);
+            $('#autocomplete-input').val('');
+        }
     });
+
+    // Search bar config
+
+    let trig = 1;
+    const searchBar = $('.expandable-search');
+    searchBar.addClass('searchbarfix');
+    //animate searchbar width increase to  +150%
+    searchBar.click(function(e) {
+        //handle other nav elements visibility here to avoid push down
+        $('.search-hide').addClass('hide');
+        if (trig === 1){
+            $('#navfix2').animate({
+                width: '+=150',
+                marginRight: 0
+            }, 400);
+
+            trig ++;
+        }
+
+    });
+
+    // if user leaves the form the width will go back to original state
+
+    searchBar.focusout(function() {
+
+        $('#navfix2').animate({
+            width: '-=150'
+        }, 400);
+        trig = trig - 1;
+        //handle other nav elements visibility first to avoid push down
+        $('.search-hide').removeClass('hide');
+    });
+
 };
 
 Template.HomeLayout.onCreated(function helloOnCreated() {
@@ -32,8 +69,9 @@ Template.Pricing.helpers({
 });
 
 Template.HomeLayout.events({
-    'submit #crypto-select-form': function() {
+    'submit #navbarsearch': function() {
         event.preventDefault();
+        console.log(event.target.text.value);
         const formVal = event.target.text.value;
         if(formVal in supportedCryptocurrencies){
             Session.set('cryptocurrecySelection', formVal);
